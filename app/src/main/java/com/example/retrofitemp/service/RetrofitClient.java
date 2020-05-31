33a8b1aae36cc405fb301a0a736c1276a4c17d5d
vilas639 <vilasjdhv639@gmail.com>
2020-05-31 15:54:47 +0530

    Update RetrofitClient.java

--- app/src/main/java/com/example/retrofitemp/service/RetrofitClient.java
+++ app/src/main/java/com/example/retrofitemp/service/RetrofitClient.java
@@ -8,7 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static Retrofit retrofit = null;
-    private static String BASE_URL="http://ec2-13-126-241-226.ap-south-1.compute.amazonaws.com:8080/api/";
+   // private static String BASE_URL="http://ec2-13-126-241-226.ap-south-1.compute.amazonaws.com:8080/api/";
+    private static String BASE_URL="http://ec2-13-233-126-6.ap-south-1.compute.amazonaws.com:8080/curddemo/api/";
 
     public static EmployeesService getService(){
 
